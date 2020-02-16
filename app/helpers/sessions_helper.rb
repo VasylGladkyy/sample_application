05cd5c54efaa90759f1f095c0732784e960700ed
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-02-16 18:23:55 +0200

    Add forgetting users

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,4 @@
 module SessionsHelper
-  def log_in(user)
-    session[:user_id] = user.id
-  end
-
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
-
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -24,6 +9,28 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
+  
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   def logged_in?
