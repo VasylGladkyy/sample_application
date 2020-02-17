d2d2031f3e1cb837c7870825f49126a44a09b61c
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-02-17 15:44:41 +0200

    Add update action to Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = user
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -18,8 +22,14 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
+  def update
     @user = user
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
