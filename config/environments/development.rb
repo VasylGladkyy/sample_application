07da069596253826bd9c280e8e39baf3f5dc19db
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-02-20 22:07:25 +0200

    Update production and development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,16 +20,11 @@ Rails.application.configure do
   end
   
   config.active_storage.service = :local
-  
+
   config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.perform_caching = false
-
-  config.action_mailer.delivery_method = :test
-
-  host = 'localhost:3000'
-  
-  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
   config.active_support.deprecation = :log
 
