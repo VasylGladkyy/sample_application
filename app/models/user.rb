40c7d30dcd92bc868173ec750c430d9878314a05
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-02-20 13:23:37 +0200

    Adding account code to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save { email.downcase! }
+  before_create :create_activation_digest
+  before_save :downcase_email
 
   validates :name, length: { maximum: 50 }, presence: true
 
@@ -15,7 +16,7 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }, allow_blank: true
 
   class << self
-    
+
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                  BCrypt::Engine.cost
@@ -39,5 +40,16 @@ class User < ApplicationRecord
   def authenticated?(remember_token)
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
