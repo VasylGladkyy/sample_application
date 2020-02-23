6c159cfb395bf0f82c3348ab949861bf6b9913a2
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2020-02-23 12:40:21 +0200

    Implement image uploading

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,7 +1,16 @@
 class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+
+=begin
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+=end
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -22,8 +31,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
+  # process scale: [400, 400]
+  
   # def scale(width, height)
   #   # do something
   # end
@@ -35,9 +44,9 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
